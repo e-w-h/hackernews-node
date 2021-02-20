3c09c58f64280a417c76ed6b42aa50531ad0cd11
E H <erichou24@gmail.com>
2021-02-20 09:38:06 -0500

    extend graphql schema with new root field

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `
 
