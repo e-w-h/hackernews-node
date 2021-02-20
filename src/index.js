5f41dcb9e24b765067272d0bfedd356f1959b60e
E H <erichou24@gmail.com>
2021-02-20 09:41:44 -0500

    add resolver for new root fields

--- src/index.js
+++ src/index.js
@@ -23,7 +23,13 @@ let links = [{
 // Implementation of the GraphQL schema
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
